19eec3161c3f357d60cef3a38741907dc19f1487
Midhun Balan <midhun.balan@anywhere.co>
2021-06-21 12:24:21 +0530

    added error valiation

--- src/main/java/com/projectmanagement/web/ProjectController.java
+++ src/main/java/com/projectmanagement/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.projectmanagement.web;
 
 import com.projectmanagement.domain.Project;
+import com.projectmanagement.services.MapValidationErrorService;
 import com.projectmanagement.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -21,11 +24,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-        if(result.hasErrors()){
-            return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap= mapValidationErrorService.mapValidationService(result);
+        if (errorMap !=null) return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
